676ba80bb1e9d3ff9627430b9265a3f8146afec7
luedman <Lukas@LUKASDESKTOP>
2023-08-22 21:04:11 +0200

    minor updates

--- nODEDRL/DeepODERL.py
+++ nODEDRL/DeepODERL.py
@@ -14,49 +14,47 @@ logging.basicConfig(filename='logfile.log', encoding='utf-8', level=logging.DEBU
 torch.manual_seed(1)
 random.seed(1)
 
-
 in_colab = 'google.colab' in sys.modules
 rcParams.update({'figure.autolayout': True})
 
-env_label = 'CartPole-v1'
+env_label = 'MountainCarContinuous-v0'
 hp1 = HyperParameterWrapper(env_label=env_label,
-                           model_class_label='nODENet',
-                           no_nodes=64,
-                           learning_mode='eps_decay_linear',
-                           no_dsteps=3,
-                           epsilon_start=1.0,
-                           epsilon_end=0.05,
-                           learning_rate=0.01,
-                           no_epochs=1000,
-                           gamma=0.99,
-                           device_str="cpu",
-                           batch_size=128,
-                           tau=0.005,
-                           label=env_label,
-                           action_dpoints=5,
-                           colab=in_colab)
-
-hp2 = HyperParameterWrapper(env_label=env_label,
                             model_class_label='nODENet',
-                            no_nodes=64,
+                            no_nodes=128,
                             learning_mode='eps_decay_linear',
-                            no_dsteps=10,
+                            no_dsteps=30,
                             epsilon_start=1.0,
-                            epsilon_end=0.05,
-                            learning_rate=0.01,
-                            no_epochs=1000,
+                            epsilon_end=0.00,
+                            learning_rate=0.0001,
+                            no_epochs=3000,
                             gamma=0.99,
                             device_str="cpu",
                             batch_size=128,
                             tau=0.005,
-                            label=env_label,
+                            label=env_label + "-d30",
+                            action_dpoints=5,
+                            colab=in_colab)
+
+hp2 = HyperParameterWrapper(env_label=env_label,
+                            model_class_label='nODENet',
+                            no_nodes=128,
+                            learning_mode='eps_decay_linear',
+                            no_dsteps=5,
+                            epsilon_start=1.0,
+                            epsilon_end=0.00,
+                            learning_rate=0.0001,
+                            no_epochs=3000,
+                            gamma=0.99,
+                            device_str="cpu",
+                            batch_size=128,
+                            tau=0.005,
+                            label=env_label + "-d5",
                             action_dpoints=5,
                             colab=in_colab)
 
 
 def run_experiment(hp):
     hp.env = gym.make(env_label)
-    state, info = hp.env.reset()
     run_model(hp, run_training=True, start_episode=1)
     run_model(hp, run_training=False)
 
